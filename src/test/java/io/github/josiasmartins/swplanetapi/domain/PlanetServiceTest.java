9f3c1965dfa324ad47fd1f7aec2211801ee2d4f9
josias.m.caitano <josiasmartinscaitano@gmail.com>
2023-09-19 18:59:51 -0300

    feat: utilizando mockito para os testes de unidade

--- src/test/java/io/github/josiasmartins/swplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/io/github/josiasmartins/swplanetapi/domain/PlanetServiceTest.java
@@ -1,24 +1,43 @@
 package io.github.josiasmartins.swplanetapi.domain;
 
+import static org.mockito.Mockito.when;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import io.github.josiasmartins.swplanetapi.common.PlanetConstants;
 
-@SpringBootTest(classes = PlanetService.class)
+//@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
 
-    @Autowired
+    // @Autowired
+    @InjectMocks // instancia uma instancia real 
     private PlanetService planetService;
+
+    // @MockBean
+    @Mock
+    private PlanetRepository planetRepository;
     
     // operacao_estado_retorno
     @Test
     public void createPlanet_WithValidData_ReturnsPlanet() {
+        // AAA
+        // Arange - arruma os dados
+        when(planetRepository.save(PlanetConstants.PLANET)).thenReturn(PlanetConstants.PLANET);
+        
+        // action - acao
         // system under test
         Planet sut = planetService.create(PlanetConstants.PLANET);
 
+        // assert
         Assertions.assertThat(sut).isEqualTo(PlanetConstants.PLANET);
     }   
 
