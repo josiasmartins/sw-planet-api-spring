01bcfc8b7f257e939c1540bd37cc0f25a308bbb0
josias.m.caitano <josiasmartinscaitano@gmail.com>
2023-09-30 19:15:34 -0300

    chore: testes de cobertura com Jacoco

--- src/main/java/io/github/josiasmartins/swplanetapi/domain/QueryBuilder.java
+++ src/main/java/io/github/josiasmartins/swplanetapi/domain/QueryBuilder.java
@@ -4,6 +4,8 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 
 public class QueryBuilder {
+
+    private QueryBuilder() {}
     
     public static Example<Planet> makeQuery(Planet planet) {
         ExampleMatcher exampleMatcher = ExampleMatcher
---
+++ src/main/java/io/github/josiasmartins/swplanetapi/jacoco/ExcludedFromJacocoGeneratedReport.java
@@ -0,0 +1,14 @@
+package io.github.josiasmartins.swplanetapi.jacoco;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ExcludedFromJacocoGeneratedReport {
+
+
+
+}
